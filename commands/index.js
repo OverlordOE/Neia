95180c36503ecced539f2037dfcd4316d26fb0fd
Daylan <daylandelange@gmail.com>
2020-03-05 00:00:33 +0100

    added reload command for easy changes

--- commands/index.js
+++ commands/index.js
@@ -2,5 +2,6 @@ module.exports = {
 	Ping: require('./ping'),
 	Avatar: require('./avatar'),
 	Help: require('./help'),
-	Hackedandbanned: require('./hackedandbanned')
+	Hackedandbanned: require('./hackedandbanned'),
+	Reload: require('./reload')
 };
