65cdf13366c7f2be4369141e8a905b16443689c7
Daylan <daylandelange@gmail.com>
2020-03-27 13:08:33 +0100

    hackandban easter egg

--- commands/hackedandbanned.js
+++ commands/hackedandbanned.js
@@ -7,8 +7,9 @@ module.exports = {
 	usage: 'user',
 	execute(msg, args) {
 
-
+		
 		const hacked = msg.mentions.users.map(user => {
+			if (user.id == 137920111754346496 || 684458276129079320) return `<@${msg.author.id}>, you cannot hack nor ban a supreme being.`;
 			return `${user.username} imma be nice to you this time but next time you gonna get HACKED AND BANNED`;
 		});
 
