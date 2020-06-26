cca342bda122bea689340f78263fd4fe0d1af4ba
Daylan <daylandelange@gmail.com>
2020-06-26 18:31:01 +0200

    small improvements to leaderboard

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -12,7 +12,6 @@ module.exports = {
 
 	async execute(msg, args, profile, bot) {
 
-
 		const filter = (reaction, user) => {
 			return ['◀️', '▶️'].includes(reaction.emoji.name) && user.id === msg.author.id;
 		};
@@ -26,9 +25,9 @@ module.exports = {
 			.map((user, position) => `\n__**${position + 1}.**__ ${(bot.users.cache.get(user.user_id))}: **${Math.floor(user.balance)}💰**`);
 
 		let page = 0;
+		if (!isNaN(args[0]) && args[0] > 0 && args[0] < 6) page = args[0] - 1;
+
 		const description = editDescription(list, page);
-
-
 
 		const embed = new Discord.MessageEmbed()
 			.setTitle('Neija leaderboard')
@@ -63,7 +62,10 @@ module.exports = {
 };
 
 function editDescription(list, page) {
-	let description;
-	for (let i = page * 10; i < (10 + page * 10); i++) description += list[i];
+	let description = '';
+	for (let i = page * 10; i < (10 + page * 10); i++) {
+		if (list[i]) description += list[i];
+		else description += `\n__**${i + 1}.**__ noone`;
+	}
 	return description;
 }
