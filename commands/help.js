11985651d4cdd1de4ac2f2e06b27ed0ae9381bf5
Daylan <daylandelange@gmail.com>
2020-06-28 18:51:32 +0200

    last commit

--- commands/help.js
+++ commands/help.js
@@ -50,8 +50,13 @@ module.exports = {
 								__**Miscellaneous Commands**__\n${miscCommands}\n
 								__**Music Commands**__\n${musicCommands}\n
 								__**Admin Commands**__\n${adminCommands}\n
-								`);
-			help.addField('__**Help**__', '**You can send `help [command name]` to get info on a specific command!**');
+								`)
+			.addField('__**Help**__', '**You can send `help [command name]` to get info on a specific command!**')
+				.addField('Helpfull Links',`[Click here to invite me to your server](https://discord.com/oauth2/authorize?client_id=684458276129079320&scope=bot&permissions=372517968)\n
+							 [Click here to join the support server](https://discord.gg/hFGxVDT)\n
+							 [Click here to submit a bug or request  feature](https://github.com/OverlordOE/Neia/issues/new/choose)\n
+							 For more info contact: OverlordOE#0717
+			`);
 		}
 		else {
 			const name = args[0].toLowerCase();
