f56de38c4325f5f25eb48c2a2f2d5eec61c07cdb
Daylan <daylandelange@gmail.com>
2020-10-09 12:52:16 +0200

    attack command now uses an embed

--- commands/attack.js
+++ commands/attack.js
@@ -11,39 +11,48 @@ module.exports = {
 
 	async execute(message, args, msgUser, profile, guildProfile, client, logger, cooldowns) {
 
-		const targetMention = message.mentions.users.first();
-		if (!targetMention) return message.reply('mention the user you want to attack.');
+		const embed = new Discord.MessageEmbed()
+			.setTitle('Neia Attacking')
+			.setThumbnail(message.author.displayAvatarURL())
+			.setTimestamp()
+			.setFooter('Neia Imporium', client.user.displayAvatarURL());
 
-		const lastAttack = await profile.getAttack(message.author.id);
-		if (lastAttack !== true) return message.reply(`your attack is on cooldown. Your next attack is available at ${lastAttack}`);
+		message.channel.send(embed.setDescription(embed)).then(async sentMessage => {
 
-		const target = await profile.getUser(targetMention.id);
-		if (target.networth < 30000) return message.reply('the target user needs to have a networth of atleast 30k to be attacked.');
-		if (msgUser.networth < 30000) return message.reply('you need to have a networth of atleast 30k to attack someone.');
+			const targetMention = message.mentions.users.first();
+			if (!targetMention) return sentMessage.edit(embed.setDescription('mention the user you want to attack.'));
 
-		const protection = await profile.getProtection(targetMention.id);
-		if (protection !== false) return message.channel.send(`*${targetMention}* has protection against attacks, you cannot attack them untill ${protection}.`);
+			const lastAttack = await profile.getAttack(message.author.id);
+			if (lastAttack !== true) return sentMessage.edit(embed.setdescription(`your attack is on cooldown. Your next attack is available at ${lastAttack}`));
+
+			const target = await profile.getUser(targetMention.id);
+			if (target.networth < 30000) return sentMessage.edit(embed.setdescription('the target user needs to have a networth of atleast 30k to be attacked.'));
+			if (msgUser.networth < 30000) return sentMessage.edit(embed.setdescription('you need to have a networth of atleast 30k to attack someone.'));
+
+			const protection = await profile.getProtection(targetMention.id);
+			if (protection !== false) return sentMessage.edit(embed.setDescription(`*${targetMention}* has protection against attacks, you cannot attack them untill ${protection}.`));
 
 
-		const equipment = await profile.getEquipment(message.author.id);
-		let damage = Math.round(1 + (Math.random() * 4));
-		if (equipment['weapon']) {
-			const weapon = profile.getItem(equipment['weapon']);
-			damage = Math.round(weapon.damage[0] + (Math.random() * weapon.damage[1]));
-			message.channel.send(`You have attacked ${targetMention} with your ${weapon.emoji}${weapon.name} for ${damage} damage`);
-		}
-		else message.channel.send(`You have attacked ${targetMention} with your fists for ${damage} damage`);
+			const equipment = await profile.getEquipment(message.author.id);
+			let damage = Math.round(1 + (Math.random() * 4));
+			if (equipment['weapon']) {
+				const weapon = profile.getItem(equipment['weapon']);
+				damage = Math.round(weapon.damage[0] + (Math.random() * weapon.damage[1]));
+				sentMessage.edit(embed.setDescription(`You have attacked ${targetMention} with your ${weapon.emoji}${weapon.name} for ${damage} damage`));
+			}
+			else sentMessage.edit(embed.setDescription(`You have attacked ${targetMention} with your fists for ${damage} damage`));
 
-		target.hp -= damage;
-		profile.setAttack(message.author.id);
+			target.hp -= damage;
+			profile.setAttack(message.author.id);
 
-		if (target.hp <= 0) {
-			const stealAmount = Math.floor(target.balance / (Math.random() + 4));
-			profile.addMoney(targetMention.id, -stealAmount);
-			profile.addMoney(message.author.id, stealAmount);
-			message.channel.send(`You have killed ${targetMention} and stolen ${profile.formatNumber(stealAmount)}💰.`);
-			profile.addProtection(targetMention.id, 24);
-			target.hp = 1000;
-		}
+			if (target.hp <= 0) {
+				const stealAmount = Math.floor(target.balance / (Math.random() + 4));
+				profile.addMoney(targetMention.id, -stealAmount);
+				profile.addMoney(message.author.id, stealAmount);
+				sentMessage.edit(embed.setDescription(`You have killed ${targetMention} and stolen ${profile.formatNumber(stealAmount)}💰.`));
+				profile.addProtection(targetMention.id, 24);
+				target.hp = 1000;
+			}
+		});
 	},
 };
