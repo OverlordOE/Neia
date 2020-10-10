4648997a55ea90453241aa103d663e6523a38157
Overlord <daylandelange@gmail.com>
2020-10-10 17:45:43 +0200

    added healing potions to lootboxes

--- data/loottables.js
+++ data/loottables.js
@@ -17,10 +17,12 @@ module.exports = {
 		// cost 3k
 		const loot = new LootTable();
 		loot.add({ name: 'Gun', amount: [1, 0] }, 10);
-		loot.add({ name: 'Motorcycle', amount: [6, 3] }, 20);
+		loot.add({ name: 'Healing Potion', amount: [1, 0] }, 10);
+		loot.add({ name: 'Healing Potion', amount: [2, 0] }, 1);
+		loot.add({ name: 'Motorcycle', amount: [6, 3] }, 15);
 		loot.add({ name: 'Car', amount: [3, 1] }, 21);
 		loot.add({ name: 'Sailboat', amount: [1, 0] }, 10);
-		loot.add({ name: 'Motorboat', amount: [2, 1] }, 20);
+		loot.add({ name: 'Motorboat', amount: [2, 1] }, 14);
 		loot.add({ name: 'Prop plane', amount: [1, 1] }, 10);
 		loot.add({ name: 'Ship', amount: [1, 0] }, 3);
 		loot.add({ name: 'Jet plane', amount: [1, 0] }, 1);
@@ -32,14 +34,15 @@ module.exports = {
 	epic() {
 		// cost 12.5k
 		const loot = new LootTable();
-		loot.add({ name: 'Shortbow', amount: [2, 0] }, 8);
+		loot.add({ name: 'Shortbow', amount: [2, 0] }, 7);
+		loot.add({ name: 'Healing Potion', amount: [9, 2] }, 7);
 		loot.add({ name: 'Water', amount: [1, 0] }, 1);
-		loot.add({ name: 'Sailboat', amount: [5, 4] }, 8);
-		loot.add({ name: 'Training Sword', amount: [3, 1] }, 8);
-		loot.add({ name: 'Training Staff', amount: [3, 1] }, 8);
-		loot.add({ name: 'Gun', amount: [2, 1] }, 8);
-		loot.add({ name: 'Motorboat', amount: [8, 3] }, 8);
-		loot.add({ name: 'Ship', amount: [2, 1] }, 10);
+		loot.add({ name: 'Sailboat', amount: [5, 4] }, 7);
+		loot.add({ name: 'Training Sword', amount: [3, 1] }, 7);
+		loot.add({ name: 'Training Staff', amount: [3, 1] }, 7);
+		loot.add({ name: 'Gun', amount: [2, 1] }, 7);
+		loot.add({ name: 'Motorboat', amount: [8, 3] }, 7);
+		loot.add({ name: 'Ship', amount: [2, 1] }, 9);
 		loot.add({ name: 'Jet Plane', amount: [2, 1] }, 8);
 		loot.add({ name: 'Prop Plane', amount: [3, 3] }, 8);
 		loot.add({ name: 'House', amount: [1, 1] }, 10);
