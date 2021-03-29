c095336a5c07c38e7bbbe37dd644baa8d947dcc9
Daylan <daylandelange@gmail.com>
2021-03-29 15:24:58 +0200

    play command will now search an additional time if no search results where found

--- commands/play.js
+++ commands/play.js
@@ -3,6 +3,7 @@ const YouTube = require('youtube-sr');
 const Discord = require('discord.js');
 const cookie = 'VISITOR_INFO1_LIVE=Uq_3Z5fqSbw; CONSENT=YES+NL.nl+20150809-08-0; PREF=f6=400&cvdm=grid&tz=Europe.Amsterdam&al=nl&f4=4000000&f5=30; __Secure-3PSID=4QefZVi5AobXAIAGnrfJyIF8ERzRkkf57JMZOsrmJKi140uppNbOdY9akSJecozb0XiSwQ.; __Secure-3PAPISID=aHbQu15SE8c0m8-t/ANemjuGFQo1a--Bzn; __Secure-3PSIDCC=AJi4QfGWI3ZKshfSQVis-7X2JYEmY_2lTOlc1ys-TRTtUb392XJ56FmizGRXTNeVYMRDv80v0RjH; LOGIN_INFO=AFmmF2swRQIhALWX7nT2GjN6k1fFYACwgnJ5CXP_sGwSe3asRq_ecTmiAiBHdD_azdvFTCkngaxt41vxYohZ9yGeggs6aiFNSSq_qA:QUQ3MjNmd2xsTzQ5dmcxdjc4SWExS3I3M0MxbFpvRmdmcGtZQzlfYlBNVFFzQnlCYTRYWExxeTZPakpLT2oyZHFnU2JBaTRBNmU1V3JVblpUZ2YzZ0dOX0JONjlYd2l0cTFCbkVWR1QzLXRsQkZRcXJDX2k5V0FVUkhfM2thQUw0VlFWVmVLVkpuNVIwall5RzF2bTBOY1VTaFI3U0Ixd3I2ckMtdk5ieEVoZUNRNTRkaXd2Z0VScXA5SVZaNzVhWHJ2OXYxaEdqQXRoNnMzSG13SnRVY1pDZWJNTFV0Z2JUMFFkemxrMzF1d1R2bzAzdms1NWx0ZFVxRUdPQmE3RlU3Wld3M2tqcGpOSA==; wide=1; YSC=--1m74C2ZYQ';
 const youtubeID = 'QUFFLUhqa3JlLTlLTkY4MVRYLUVqLTdrRFVfRDBsOGp1QXw\u003d';
+let hasSearched = false;
 
 module.exports = {
 	name: 'Play',
@@ -16,7 +17,7 @@ module.exports = {
 
 	async execute(message, args, msgUser, client, logger) {
 
-		const embed = new Discord.MessageEmbed()
+		let embed = new Discord.MessageEmbed()
 			.setThumbnail(message.author.displayAvatarURL({ dynamic: true }));
 
 		if (!message.member.voice.channel) return message.channel.send(embed.setDescription('you are not in a voice channel.'));
@@ -26,19 +27,19 @@ module.exports = {
 
 		try {
 			if (!data.connection) {
-				
+
 				const permissions = message.member.voice.channel.permissionsFor(message.guild.member(client.user));
 				if (!permissions.any('CONNECT')) {
 					logger.warn('Neia couldnt join the voice channel');
 					return message.reply('Neia does not have permission to join the voice channel!');
 				}
-				
+
 			}
 			else if (data.connection.status == 4 && data.queue) {
 				data.dispatcher = null;
 				data.queue.shift();
 			}
-			
+
 			data.connection = await message.member.voice.channel.join();
 		}
 		catch (error) {
@@ -52,123 +53,141 @@ module.exports = {
 		data.guildID = message.guild.id;
 
 		const tempMessage = await message.channel.send('Finding youtube video...');
-		let video;
 
-		if (ytdl.validateURL(search)) {
-			const info = await ytdl.getBasicInfo(search, {
+		const video = await SearchVideo();
+		tempMessage.delete();
+
+		if (!video) {
+			logger.warn(`Could not find youtube video with search terms "${search}"`);
+			return message.channel.send(embed.setDescription(`Neia could not find any video connected to the search terms of \`${search}\``));
+		}
+
+
+		if (!data.dispatcher) Play(client, data, logger, msgUser, message);
+		else message.channel.send(embed.setDescription(`**${video.title}**\nBy **${video.channel}**\n Has been added to the queue.\n\nRequested by ${message.author}`));
+
+		client.music.active.set(message.guild.id, data);
+
+
+		async function SearchVideo() {
+			let videoData;
+			let searchData;
+
+			if (ytdl.validateURL(search)) {
+				const info = await ytdl.getBasicInfo(search, {
+					requestOptions: {
+						headers: {
+							Cookie: cookie,
+							'x-youtube-identity-token': youtubeID,
+						},
+					},
+				});
+				searchData = info.videoDetails;
+				let duration = 0;
+
+				const minutes = Math.floor(searchData.lengthSeconds / 60);
+				const seconds = searchData.lengthSeconds - (minutes * 60);
+				if (seconds < 10) duration = `${minutes}:0${seconds}`;
+				else duration = `${minutes}:${seconds}`;
+
+				videoData = {
+					title: searchData.title,
+					channel: searchData.author,
+					requester: message.author,
+					url: searchData.video_url,
+					announceChannel: message.channel.id,
+					duration: duration,
+					thumbnail: searchData.thumbnails[0].url,
+				};
+				data.queue.push(videoData);
+				embed.setThumbnail(videoData.thumbnail);
+			}
+			else {
+				searchData = await YouTube.searchOne(search);
+				if (searchData) {
+					videoData = {
+						title: searchData.title,
+						channel: searchData.channel.name,
+						requester: message.author,
+						url: searchData.url,
+						announceChannel: message.channel.id,
+						duration: searchData.durationFormatted,
+						thumbnail: searchData.thumbnail.url,
+					};
+					data.queue.push(videoData);
+					embed.setThumbnail(videoData.thumbnail);
+				}
+
+				else if (hasSearched) return null;
+				else {
+					logger.warn('Search 1 has failed');
+					hasSearched = true;
+					await SearchVideo();
+				}
+			}
+
+			return videoData;
+		}
+
+
+		async function Play() {
+			data.paused = false;
+			const channel = client.channels.cache.get(data.queue[0].announceChannel);
+			embed = new Discord.MessageEmbed()
+				.setThumbnail(data.queue[0].thumbnail);
+
+			channel.send(embed.setDescription(`Now playing **${data.queue[0].title}**\nBy **${data.queue[0].channel}**\n\nRequested by ${data.queue[0].requester}`));
+
+			data.dispatcher = data.connection.play(ytdl(data.queue[0].url, {
 				requestOptions: {
 					headers: {
 						Cookie: cookie,
 						'x-youtube-identity-token': youtubeID,
 					},
 				},
+				filter: 'audioonly',
+				highWaterMark: 1 << 25,
+				opusEncoded: true,
+			}), {
+				type: 'opus',
+				bitrate: 'auto',
 			});
-			video = info.videoDetails;
-			let duration = 0;
+			data.dispatcher.guildID = data.guildID;
 
-			const minutes = Math.floor(video.lengthSeconds / 60);
-			const seconds = video.lengthSeconds - (minutes * 60);
-			if (seconds < 10) duration = `${minutes}:0${seconds}`;
-			else duration = `${minutes}:${seconds}`;
+
+			data.dispatcher.on('finish', () => Finish(client, data.dispatcher, logger, msgUser, message));
+			data.dispatcher.on('debug', e => {
+				channel.send(embed.setDescription(`error:  ${e.info}`));
+				logger.error(e);
+			});
+
+			data.dispatcher.on('disconnect', e => {
+				data.queue = [];
+				data.dispatcher.emit('finish');
+				logger.log('info', `Bot got forcefully disconnected by: ${e.info}}`);
+			});
+
+		}
+
+
+		function Finish() {
+
 			
-			const videoData = {
-				title: video.title,
-				channel: video.author,
-				requester: message.author,
-				url: video.video_url,
-				announceChannel: message.channel.id,
-				duration: duration,
-				thumbnail: video.thumbnails[0].url,
-			};
-			video = videoData;
-			data.queue.push(videoData);
-			embed.setThumbnail(video.thumbnail);
-		}
-		else {
-			video = await YouTube.searchOne(search);
-			if (video) {
-				const videoData = {
-					title: video.title,
-					channel: video.channel.name,
-					requester: message.author,
-					url: video.url,
-					announceChannel: message.channel.id,
-					duration: video.durationFormatted,
-					thumbnail: video.thumbnail.url,
-				};
-				video = videoData;
-				data.queue.push(videoData);
-				embed.setThumbnail(video.thumbnail);
+			data.queue.shift();
+
+			if (data.queue.length > 0) {
+				client.music.active.set(data.dispatcher.guildID, data);
+				Play(client, data, logger, msgUser, message);
 			}
+
 			else {
-				logger.warn(`Could not find youtube video with search terms "${search}"`);
-				tempMessage.delete();
-				return message.channel.send(embed.setDescription(`Neia could not find any video connected to the search terms of \`${search}\``));
+				client.music.active.delete(data.dispatcher.guildID);
+				const voiceChannel = client.guilds.cache.get(data.dispatcher.guildID).me.voice.channel;
+				if (voiceChannel) voiceChannel.leave();
 			}
 		}
 
-		tempMessage.delete();
-		if (!data.dispatcher) Play(client, data, logger, msgUser, message);
-		else message.channel.send(embed.setDescription(`**${video.title}**\nBy **${video.channel}**\n Has been added to the queue.\n\nRequested by ${message.author}`));
 
-		client.music.active.set(message.guild.id, data);
 	},
 };
 
-
-async function Play(client, data, logger, msgUser, message) {
-	data.paused = false;
-	const channel = client.channels.cache.get(data.queue[0].announceChannel);
-	const embed = new Discord.MessageEmbed()
-		.setThumbnail(data.queue[0].thumbnail);
-
-	channel.send(embed.setDescription(`Now playing **${data.queue[0].title}**\nBy **${data.queue[0].channel}**\n\nRequested by ${data.queue[0].requester}`));
-
-	data.dispatcher = data.connection.play(ytdl(data.queue[0].url, {
-		requestOptions: {
-			headers: {
-				Cookie: cookie,
-				'x-youtube-identity-token': youtubeID,
-			},
-		},
-		filter: 'audioonly',
-		highWaterMark: 1 << 25,
-		opusEncoded: true,
-	}), {
-		type: 'opus',
-		bitrate: 'auto',
-	});
-	data.dispatcher.guildID = data.guildID;
-
-
-	data.dispatcher.on('finish', () => Finish(client, data.dispatcher, logger, msgUser, message));
-	data.dispatcher.on('debug', e => {
-		channel.send(embed.setDescription(`error:  ${e.info}`));
-		logger.error(e);
-	});
-	
-	data.dispatcher.on('disconnect', e => {
-		data.queue = [];
-		data.dispatcher.emit('finish');
-		logger.log('info', `Bot got forcefully disconnected by: ${e.info}}`);
-	});
-	
-}
-
-
-function Finish(client, dispatcher, logger, msgUser, message) {
-
-	const fetchedData = client.music.active.get(dispatcher.guildID);
-	fetchedData.queue.shift();
-
-	if (fetchedData.queue.length > 0) {
-		client.music.active.set(dispatcher.guildID, fetchedData);
-		Play(client, fetchedData, logger, msgUser, message);
-	}
-
-	else {
-		client.music.active.delete(dispatcher.guildID);
-		const voiceChannel = client.guilds.cache.get(dispatcher.guildID).me.voice.channel;
-		if (voiceChannel) voiceChannel.leave();
-	}
-}
