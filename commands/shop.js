a5aa8d49d0f46ed7de1a42aa3aa0ad6d22ee3bcf
Daylan <daylandelange@gmail.com>
2021-04-16 18:43:43 +0200

    added back the shop command

---
+++ commands/shop.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+const items = require('../data/items');
+module.exports = {
+	name: 'shop',
+	summary: 'Shows all the shop items',
+	description: 'Shows all the shop items.',
+	category: 'info',
+	aliases: ['store'],
+	args: false,
+	usage: '',
+
+	execute(message, args, msgUser, msgGuild, client, logger) {
+
+		let collectables = '__**Collectables:**__\n';
+		let chests = '__**Chests:**__\n';
+		let equipment = '__**Equipment:**__\n';
+
+		Object.values(items).sort((a, b) => a.value - b.value).map((i) => {
+			if (i.buyable) {
+				if (i.ctg == 'collectable') collectables += `${i.emoji} ${i.name}: ${client.util.formatNumber(i.value)}💰\n`;
+				else if (i.ctg == 'chest') chests += `${i.emoji} ${i.name}: ${client.util.formatNumber(i.value)}💰\n`;
+				else if (i.ctg == 'equipment') equipment += `${i.emoji}${i.name}: ${client.util.formatNumber(i.value)}💰\n`;
+			}
+		});
+
+		const description = `${chests}\n${equipment}\n${collectables}`;
+
+		const embed = new Discord.MessageEmbed()
+			.setTitle('Project Neia Shop')
+			.setThumbnail(client.user.displayAvatarURL())
+			.setDescription(description)
+			.setColor('#f3ab16')
+			.setFooter('Use the items command to see the full item list.', client.user.displayAvatarURL());
+
+		return message.channel.send(embed);
+	},
+};
