edd376ab71f678b92b58c6c2941bf1d9cd9aba88
Daylan <daylandelange@gmail.com>
2021-05-03 18:14:45 +0200

    Update changelog.js

--- commands/changelog.js
+++ commands/changelog.js
@@ -12,28 +12,33 @@ module.exports = {
 		const embed = new Discord.MessageEmbed()
 			.setTitle('Neia V3.4: Power Counting')
 			.setFooter('To see earlier updates join the support server.', client.user.displayAvatarURL({ dynamic: true }))
-			.addField('**Items**', `- New powerup: \`Power Counting\`.
-			This powerup will let you count by yourself for **1 minute**. Has a **3 hour** cooldown.
-- Added **pepeThat** reaction.
-- Added **pepeEZ** reaction.
-- Lowered **pepeBiz** value from **250k -> 200k.**
-- Lowered **Monkey** value from **200k -> 150k.**
+			.addField('**Number Game Event**', `
+			A random event will now pop-up in the designated Number Game channel randomly every hour.
+Only 1 person can claim it and when they do the bot will count around 10 numbers for them and give them all the money they would normally would gain from counting.
+If the event isn't claimed within 10 minutes it will expire.
 `)
 
 
-			.addField('**Stats**', `
-			- Split the main stats page into **2 pages**. 1 for cooldowns and balance, the other one for non important stats like __Streaks Ruined__.
-- Added stat for **Power Counting** cooldown.
+			.addField('**Items**', `
+			- Added **Pepe Money** custom reaction priced **200.1k**
 			`)
 
-			.addField('**Gambling**', `- Added some more text highlighting.
+			.addField('**Power Count**', `
+			- Changed name from \`Power Counting\` to \`Power Count\`.
+- It will now give you a warning **10 seconds** before it runs out.
+			`)
+
+			.addField('**Gambling**', `
+			- Fruit Slots: 
+A new symbol 7️⃣ has been added to replace the 🍋 . If you get a 7️⃣ row you will get **3** rows instead of **1**.
+- Gambling commands now show you how much you have bet.
+- \`Blackjack\` and \`Fruit Slots\` now show how much money you have won and lost.
 			`)
 
 
 			.addField('**Bug Fixes**', `
-			- Fixed bug where Neia wouldn't recognize the amount you put in with the \`Buy\` and \`Trade\` commands.
-- Fixed \`Item\` command not working at all.
-- Fixed bug where you couldn't buy **Streak Protection**.
+			- Fixed \`Sell\` command not working at all.
+- \`Buy\`, \`Trade\` and \`Sell\`  now change colour based on if the command was successful.
 `);
 
 		return message.channel.send(embed);
