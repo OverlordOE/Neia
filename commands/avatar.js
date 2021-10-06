25ba8d01087651fac75aae1bdd02faee28bc0866
Daylan <daylandelange@gmail.com>
2021-10-06 13:26:33 +0200

    Create avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,24 @@
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const { SlashCommandBuilder, hyperlink } = require('@discordjs/builders');
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('avatar')
+		.setDescription('Will show a preview of the avatar together with a link to download the avatar.')
+		.addUserOption(option =>
+			option
+				.setName('target')
+				.setDescription('Select a user')),
+
+	execute(interaction, msgUser, msgGuild, client, logger) {
+		const target = interaction.options.getUser('target') || interaction.user;
+		const avatarLink = target.displayAvatarURL({ dynamic: true });
+
+		const embed = new MessageEmbed()
+			.setTitle(`${target.tag}'s Avatar`)
+			.setDescription(hyperlink('Download Link', avatarLink))
+			.setImage(avatarLink)
+			.setColor('#f3ab16');
+
+		interaction.reply({ embeds: [embed] });
+	},
+};
