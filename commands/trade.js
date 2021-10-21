e0b0f4e285d25cb3ca1083b803b82ee8aee8d423
Daylan <daylandelange@gmail.com>
2021-10-21 13:18:40 +0200

    added trade command

---
+++ commands/trade.js
@@ -0,0 +1,133 @@
+const { MessageEmbed } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('trade')
+		.setDescription('Trade items or money to other people')
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('item')
+				.setDescription('Trade an item')
+				.addUserOption(option =>
+					option
+						.setName('target')
+						.setDescription('The user you want to trade too.')
+						.setRequired(true),
+				)
+				.addStringOption(option =>
+					option
+						.setName('item')
+						.setDescription('The item you want to trade.')
+						.setRequired(true),
+				)
+				.addIntegerOption(option =>
+					option
+						.setName('amount')
+						.setDescription('The amount of items you want to trade.')
+						.setRequired(true),
+				))
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('money')
+				.setDescription('Trade money')
+				.addUserOption(option =>
+					option
+						.setName('target')
+						.setDescription('The user you want to trade too.')
+						.setRequired(true),
+				)
+				.addIntegerOption(option =>
+					option
+						.setName('amount')
+						.setDescription('The amount of items you want to trade.')
+						.setRequired(true),
+				)),
+
+	async execute(interaction, msgUser, msgGuild, client, logger) {
+
+		const embed = new MessageEmbed()
+			.setTitle('Neia Trading Center')
+			.setFooter('You can only trade to people on the same server.', client.user.displayAvatarURL({ dynamic: true }))
+			.setColor('#f3ab16');
+
+		const target = interaction.options.getUser('target');
+		const targetUser = await client.userCommands.getUser(target.id);
+		let amount = interaction.options.getInteger('amount');
+		if (amount < 1) amount = 1;
+		let item;
+
+		
+		if (interaction.options.getSubcommand() === 'item') {
+			const tempItem = interaction.options.getString('item');
+			item = client.util.getItem(tempItem);
+
+			if (item) itemTrade();
+			else {
+				return interaction.reply({
+					embeds: [embed.setDescription(`__**ITEM(S) NOT TRADED!**__
+					__${tempItem}__ is not a valid item.`).setColor('#fc0303')], ephemeral: true,
+				});
+			}
+		}
+		else if (interaction.options.getSubcommand() === 'money') moneyTrade();
+
+		function moneyTrade() {
+			let balance = msgUser.balance;
+			if (amount > balance) {
+				return interaction.reply({
+					embeds: [embed.setDescription(`__**MONEY NOT TRADED!**__
+					You only have ${client.util.formatNumber(balance)}💰 but you need ${client.util.formatNumber(amount)}💰.`)
+						.setColor('#fc0303')], ephemeral: true,
+				});
+			}
+
+			balance = client.userCommands.addBalance(msgUser, -amount);
+			client.userCommands.addBalance(targetUser, amount);
+			return interaction.reply({
+				embeds: [embed.setDescription(
+					`Trade with *${target}* succesfull!\n\nTransferred ${client.util.formatNumber(amount)}💰 to *${target}*.
+				Your current balance is ${client.util.formatNumber(balance)}💰`)
+					.setColor('#00fc43')],
+			});
+		}
+
+		async function itemTrade() {
+			if (!await client.userCommands.hasItem(msgUser, item, amount)) {
+				return interaction.reply({
+					embeds: [embed.setDescription(`__**ITEM(S) NOT TRADED!**__
+					You don't have enough **${item.name}**.`)
+						.setColor('#fc0303')], ephemeral: true,
+				});
+			}
+
+			const protectionItem = client.util.getItem('streak protection');
+			if (item == protectionItem && !(await client.userCommands.newProtectionAllowed(targetUser))) {
+				return interaction.reply({
+					embeds: [embed.setDescription(`__**ITEM(S) NOT TRADED!**__
+					${target} already has a Streak Protection or it's on cooldown.`).setColor('#fc0303')], ephemeral: true,
+				});
+			}
+			if (item.ctg == 'reaction') {
+				const reaction = client.userCommands.getReaction(msgUser);
+				if (item.emoji == reaction.emoji) {
+					msgUser.reaction = JSON.stringify({
+						emoji: '✅',
+						value: 1,
+					});
+					msgUser.save();
+				}
+			}
+
+
+			client.userCommands.addItem(targetUser, item, amount);
+			client.userCommands.removeItem(msgUser, item, amount);
+			interaction.reply({
+				embeds: [embed.setDescription(`Trade with *${target}* succesfull!
+			\nTraded ${amount} ${item.emoji}__${item.name}__ to *${target}*.`)
+					.setColor('#00fc43')],
+			});
+		}
+
+	},
+};
+
